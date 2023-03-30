9b3d5b9f68ac1e0172eb86d16177ed9e389ff1ab
r-hayashi <ryo.robolabo@gmail.com>
2023-03-30 19:41:55 +0900

    change for depoying

--- src/auth/config/auth.ts
+++ src/auth/config/auth.ts
@@ -1,7 +1,7 @@
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
 
 export const initAuth = () => {
-	Amplify.configure({
+	Auth.configure({
 		aws_cognito_region: process.env.REACT_APP_AUTH_REGION,
 		aws_user_pools_id: process.env.REACT_APP_AUTH_USER_POOL_ID,
 		aws_user_pools_web_client_id: process.env.REACT_APP_AUTH_USER_POOL_WEB_CLIENT_ID
