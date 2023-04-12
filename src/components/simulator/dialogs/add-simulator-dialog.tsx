eba00bddad851380ad95ea7f9e0f42160a42cf61
ryoichi <ryo.robolabo@gmail.com>
2023-04-12 21:52:16 +0900

    add comment out
    see https://github.com/rych-hysh/amateras_frontend/wiki/Memos#1

--- src/components/simulator/dialogs/add-simulator-dialog.tsx
+++ src/components/simulator/dialogs/add-simulator-dialog.tsx
@@ -1,16 +1,29 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, TextField } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, MenuItem, Select, TextField } from "@mui/material";
 import { useState } from "react";
+import { Algorithm } from "../../../intefaces/algorithm";
 
-export function AddSimulatorDialog(props: { addSimulatorOpen: boolean; setAddSimulatorOpen: (arg0: boolean) => void; addSimulator: (name: string) => void; }) {
+export function AddSimulatorDialog(props: {
+	algorithmList: Algorithm[]; addSimulatorOpen: boolean; setAddSimulatorOpen: (arg0: boolean) => void; addSimulator: (name: string) => void; 
+}) {
 	const [simulatorName, setSimulatorName] = useState("" as string);
+	// const [algorithm, setAlgorithm] = useState({id: 1, name: ""} as Algorithm); // https://github.com/rych-hysh/amateras_frontend/wiki/Memos#1
 	if (simulatorName === undefined) setSimulatorName("");
 	const handleChange = (event: any) => {
 		setSimulatorName(event.target.value);
+		// console.log(algorithm)
 	}
 	const handleClose = () => {
 		if (simulatorName === "") props.setAddSimulatorOpen(true);
 		setSimulatorName("");
 	}
+	// https://github.com/rych-hysh/amateras_frontend/wiki/Memos#1
+	// const handleAlgorithmChange = (event: any) => {
+	// 	if(props.algorithmList.find(algorithm => algorithm.id === event.target.value) === undefined ) {
+	// 		setAlgorithm(props.algorithmList.length > 0 ? props.algorithmList[0] : {id: 0, name: ""} as Algorithm)
+	// 	}else{
+	// 		setAlgorithm(props.algorithmList.find((algorithm: Algorithm) => algorithm.id === event.target.value) as Algorithm);
+	// 	}
+	// }
 	return (
 		<Dialog open={props.addSimulatorOpen} onClose={handleClose}>
 			<DialogTitle>Add new simulator.</DialogTitle>
@@ -31,9 +44,20 @@ export function AddSimulatorDialog(props: { addSimulatorOpen: boolean; setAddSim
 				/>
 				<Divider />
 				<Divider />
-				<DialogContentText>
+				{/* https://github.com/rych-hysh/amateras_frontend/wiki/Memos#1
+				 <DialogContentText>
 					Choose simulate algorithms;
 				</DialogContentText>
+				<Select
+							id="algorithm-selector"
+							labelId="algorithm"
+							value={algorithm.id}
+							onChange={handleAlgorithmChange}
+						>
+							{props.algorithmList.map((algorithm) =>
+								<MenuItem key={algorithm.id} value={algorithm.id}>{algorithm.name} </MenuItem>
+							)}
+						</Select> */}
 			</DialogContent>
 			<DialogActions>
 				<Button onClick={() => props.setAddSimulatorOpen(false)}>Cancel</Button>
